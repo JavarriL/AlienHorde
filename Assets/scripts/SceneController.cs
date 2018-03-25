80c3cfbc355ded1a1328cf290d67221a6a07242d
Little <littl2j@cmich.edu>
2018-03-25 14:49:01 -0400

    Styled the title page, added a game and Game Over scene

---
+++ Assets/scripts/SceneController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour {
+
+	public string sceneToGoTo;
+	public Button btn;
+
+	// Use this for initialization
+	void Start () {
+		Button task = btn.GetComponent<Button> ();
+		task.onClick.AddListener (taskOnClick);
+	}
+	
+	void taskOnClick() {
+		SceneManager.LoadScene (sceneToGoTo);
+	}
+}
